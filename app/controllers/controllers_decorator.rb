5d0cc923050b06921352824704146b5ae6cd0516
Stefan Senk <stefansenk@yahoo.co.uk>
2012-09-15 11:30:29 +0100

    Move controller stuff to the controllers decorator

--- app/controllers/controllers_decorator.rb
+++ app/controllers/controllers_decorator.rb
@@ -1,3 +1,19 @@
+
+class Spree::Preferences::StoreInstance
+  # Initialize the preference without writing to the database
+  def set_without_persist(key, value)
+    @cache.write(key, value)
+  end
+end
+
+module SpreeMultiTenant
+  def self.init_preferences
+    Spree::Preference.all.each do |preference|
+      Spree::Preferences::Store.instance.set_without_persist(preference.key, preference.value)
+    end
+  end
+end
+
 SpreeMultiTenant.tenanted_controllers.each do |controller|
   controller.class_eval do
 
@@ -20,6 +36,7 @@ SpreeMultiTenant.tenanted_controllers.each do |controller|
 
         # Execute ActiveRecord queries within the scope of the tenant
         Multitenant.with_tenant tenant do
+          SpreeMultiTenant.init_preferences
           yield
         end
       end
--- app/models/models_decorator.rb
+++ app/models/models_decorator.rb
@@ -20,35 +20,3 @@ SpreeMultiTenant.tenanted_models.each do |model|
     # raise_error_if_no_tenant if Rails.env = 'production'
   end
 end
-
-
-class Spree::Preferences::StoreInstance
-  # Initialize the preference without writing to the database
-  def set_without_persist(key, value)
-    @cache.write(key, value)
-  end
-end
-
-module SpreeMultiTenant
-
-  def self.init_preferences
-    Spree::Preference.all.each do |preference|
-      Spree::Preferences::Store.instance.set_without_persist(preference.key, preference.value)
-    end
-  end
-end
-
-
-SpreeMultiTenant.tenanted_controllers.each do |controller|
-  controller.class_eval do
-
-    append_before_filter :init_preferences
-
-    private
-
-      def init_preferences
-        SpreeMultiTenant.init_preferences
-      end
-  end
-end
-
