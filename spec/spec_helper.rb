c8db83798ef47b76623e6e77a87caa3dec3ac939
Stefan Senk <stefansenk@yahoo.co.uk>
2013-01-10 11:31:45 +0000

    Remove require testing_support/env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
 
-require 'spree/core/testing_support/env'
 require 'spree/core/url_helpers'
 require 'spree/../../config/routes'
 
