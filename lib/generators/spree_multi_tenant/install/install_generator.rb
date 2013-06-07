96f4a4cacece69882dd7600510e11e24c3059774
Stefan Senk <stefan@senktec.com>
2013-06-07 16:48:24 +1200

    Honor 'auto_run_migrations' in InstallGenerator.

--- lib/generators/spree_multi_tenant/install/install_generator.rb
+++ lib/generators/spree_multi_tenant/install/install_generator.rb
@@ -2,6 +2,8 @@ module SpreeMultiTenant
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+      class_option :auto_run_migrations, :type => :boolean, :default => false
+
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_multi_tenant\n"
         append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_multi_tenant\n"
@@ -17,11 +19,11 @@ module SpreeMultiTenant
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
+         run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+         if run_migrations
            run 'bundle exec rake db:migrate'
          else
-           puts "Skiping rake db:migrate, don't forget to run it!"
+           puts 'Skipping rake db:migrate, don\'t forget to run it!'
          end
       end
     end
